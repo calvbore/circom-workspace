11902aa249f74afbfde004c85e7436c7ffb69d39
Calvin <calv.bore@gmail.com>
2021-03-12 01:04:29 -0600

    modified compile scripts and folder structure

--- scripts/compile.js
+++ scripts/compile.js
@@ -31,14 +31,16 @@ for (circuitName of circuitsList.split(',')) {
   // doesnt catch yet
   // https://github.com/iden3/snarkjs/pull/75
   try {
-    execSync('npx circom circuit.circom --r1cs --wasm --sym', {
-      stdio: 'inherit',
-    });
-    execSync('npx snarkjs r1cs info circuit.r1cs', { stdio: 'inherit' });
     execSync(
-      'npx snarkjs zkey new circuit.r1cs pot15_final.ptau circuit_' +
-        circuitName +
-        '.zkey',
+      'npx circom circuit.circom --r1cs --wasm --sym',
+      { stdio: 'inherit' }
+    );
+    execSync(
+      'npx snarkjs r1cs info circuit.r1cs',
+      { stdio: 'inherit' }
+    );
+    execSync(
+      'npx snarkjs zkey new circuit.r1cs pot15_final.ptau circuit_' + circuitName + '.zkey',
       { stdio: 'inherit' }
     );
     if (deterministic) {
@@ -77,6 +79,7 @@ for (circuitName of circuitsList.split(',')) {
       'npx snarkjs groth16 verify verification_key.json public.json proof.json',
       { stdio: 'inherit' }
     );
+
     execSync(
       'mkdir -p ./compiled/',
       { stdio: 'inherit' }
@@ -85,17 +88,30 @@ for (circuitName of circuitsList.split(',')) {
       'mkdir -p ./keys/',
       { stdio: 'inherit' }
     );
+    execSync(
+      'mkdir -p ./contracts/',
+      { stdio: 'inherit' }
+    );
+
     fs.copyFileSync(
       'circuit.wasm',
       cwd + '/circuits/' + circuitName + '/' + wasmOutPath + '/circuit.wasm'
     );
+    fs.unlinkSync('circuit.wasm');
     fs.copyFileSync(
       'circuit.zkey',
       cwd + '/circuits/' + circuitName + '/' + zkeyOutPath + '/circuit_final.zkey'
     );
+    fs.unlinkSync('circuit.zkey');
     fs.copyFileSync(
       'verification_key.json',
       cwd + '/circuits/' + circuitName + '/' + zkeyOutPath + '/verification_key.json'
+    );
+    fs.unlinkSync('verification_key.json');
+    
+    execSync(
+      'npx snarkjs zkey export solidityverifier keys/circuit_final.zkey contracts/verifier.sol',
+      { stdio: 'inherit' }
     );
   } catch (error) {
     console.log(error);
